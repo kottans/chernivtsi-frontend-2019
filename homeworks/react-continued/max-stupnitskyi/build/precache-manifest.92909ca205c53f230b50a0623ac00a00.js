b6de693a6f4f4b6b06257119a0e508c98f4f2abe
Max Stupnitskyi <maxstupnitskyi@gmail.com>
2019-05-23 01:57:10 +0300

    fix folder structure

---
+++ homeworks/react-continued/max-stupnitskyi/build/precache-manifest.92909ca205c53f230b50a0623ac00a00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e70e5d895cbcd760084dfdf8eafa36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ac1d4c0b76bcabead9c",
+    "url": "/static/css/main.f26ecf9d.chunk.css"
+  },
+  {
+    "revision": "fd2041e034f334722d67",
+    "url": "/static/js/2.287283f5.chunk.js"
+  },
+  {
+    "revision": "2ac1d4c0b76bcabead9c",
+    "url": "/static/js/main.ccb523aa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/static/media/github-logo.76d344f0.svg"
+  }
+]);
