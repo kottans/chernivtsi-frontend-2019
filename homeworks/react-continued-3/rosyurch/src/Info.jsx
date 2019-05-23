d3e78dbf9d99fe71800b3c5e98c11a98dfebf2a6
Rostyslav Yurchyk <ros_ur@ukr.net>
2019-05-23 18:19:42 +0300

    react part 3 homework

---
+++ homeworks/react-continued-3/rosyurch/src/Info.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+function Info (props) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    function toggle () {
+        setIsOpen(!isOpen);
+    }
+
+    return (
+        <div>
+            <p>{props.title} <button onClick={toggle}>{!isOpen ? '+' : '-'}</button> </p>
+            {isOpen &&
+                <>
+                    <p>When: {props.date} </p><p> Lecturer: {props.lecturer}</p>
+                    <p>{ props.link && <a href = {props.link}>Link</a> } </p> 
+                </>
+            }
+        </div>
+    )
+}
+
+export default Info;
