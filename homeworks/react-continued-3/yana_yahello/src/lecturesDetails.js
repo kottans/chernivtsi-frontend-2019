84b1e91d2cf2c3d528fa347fddb85b613e5fa1b5
Yana Yahello <yahellonchik@gmail.com>
2019-05-26 17:14:22 +0300

    react-continued-3

---
+++ homeworks/react-continued-3/yana_yahello/src/lecturesDetails.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./App.css";
+import {useState} from 'react';
+import PropTypes from 'prop-types';
+
+function LectureDetails({date, title, lecturer, link}) {
+    LectureDetails.propTypes = {
+        date: PropTypes.string,
+        title: PropTypes.string,
+        lecturer: PropTypes.string,
+        link: PropTypes.string
+    };
+
+    const [isOpened, setState] = useState(false);
+
+    return (
+        <div>
+            <h2 onClick={() => setState(!isOpened)}>Date: {date}</h2>
+            {isOpened && <div>
+                <p><span>Title:</span> {title}</p>
+                <p><span>Lecturer:</span> {lecturer}</p>
+                <p><span>Link:</span> {link}</p>
+            </div>}
+        </div>
+    )
+}
+
+
+export default LectureDetails
