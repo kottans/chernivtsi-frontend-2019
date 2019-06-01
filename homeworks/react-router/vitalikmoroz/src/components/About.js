650275893151bcbd15dc9a35c3e7cd303c282dd2
Vitalik Moroz <vitalik.moroz2@gmail.com>
2019-06-01 10:08:40 +0300

    React-Router

---
+++ homeworks/react-router/vitalikmoroz/src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+    return(
+        <div className="about">
+            <h1>About Page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit earum laborum reprehenderit ex, sequi, quasi delectus quis sapiente placeat inventore facere laudantium ullam repellendus expedita minus corporis pariatur eligendi. Eveniet!</p>
+        </div>
+    )
+}
